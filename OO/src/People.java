1f8fb7c2279e5bee55fb09a157b3d0b768d69a34
ONatanFreitas <onatanfreitas@icloud.com>
2022-08-25 22:04:01 -0300

    oo-exercice

---
+++ OO/src/People.java
@@ -0,0 +1,18 @@
+
+public class People {
+	private String firstName;
+	private String lastName;
+	private String address;
+	private int age;
+	private String rg;
+	private String cpf;
+	private char sex;
+}
+
+class Teatcher extends People {
+	private int register;
+}
+
+class Student extends People {
+	private int ra;
+}
